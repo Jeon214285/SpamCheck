cd554071991f43815c205320641b70c65aa8b09c
Jeon <214285@jnu.ac.kr>
2026-05-14 14:00:49 +0900

    ci: add mlflow

--- ml/train.py
+++ ml/train.py
@@ -6,13 +6,18 @@ import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import mlflow.sklearn
 
 BASE_DIR = os.path.dirname(__file__)
-DATA_PATH = os.path.join(BASE_DIR, "data", "spam.csv")
+DATA_PATH = os.path.join(BASE_DIR, "data", "spam_short.csv")
 ARTIFACT_DIR = os.path.join(BASE_DIR, "artifacts")
 MODEL_PATH = os.path.join(ARTIFACT_DIR, "spam_model.joblib")
 
 os.makedirs(ARTIFACT_DIR, exist_ok=True)
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("spam_classification-local")
 
 df = pd.read_csv(DATA_PATH)
 
@@ -24,8 +29,29 @@ pipeline = Pipeline([
     ("classifier", LogisticRegression(max_iter=200))
 ])
 
-pipeline.fit(X, y)
+with mlflow.start_run():
+    # 실험설정기록
+    mlflow.log_param("model_type", "LogisticRegression")
+    mlflow.log_param("vectorizer", "CountVectorizer")
+    mlflow.log_param("max_iter", 200)
+    mlflow.log_param("data_path", DATA_PATH)
+    mlflow.log_param("row_count", len(df))
 
-joblib.dump(pipeline, MODEL_PATH)
+    pipeline.fit(X, y)
 
-print(f"Model saved to: {MODEL_PATH}")
+    # 간단한 metric 저장(train accuracy)
+    preds = pipeline.predict(X)
+    acc = accuracy_score(y, preds)
+    mlflow.log_metric("train_accuracy", acc)
+
+    joblib.dump(pipeline, MODEL_PATH)
+
+    # artifact 기록
+    mlflow.log_artifact(DATA_PATH) # 데이터 = 실무에서는 데이터는 따로 관리
+    mlflow.log_artifact(MODEL_PATH) # 모델파일
+
+    # MLflow 모델 형식으로도 저장
+    mlflow.sklearn.log_model(pipeline, name="model")
+
+    print(f"Model saved to: {MODEL_PATH}")
+    print(f"train_accuracy: {acc:.4f}")
