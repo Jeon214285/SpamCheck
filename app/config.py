eb520bafa44b2b419e874026d9be4d765f571550
Jeon <214285@jnu.ac.kr>
2026-05-14 14:19:13 +0900

    feature: add load model from mlflow

--- app/config.py
+++ app/config.py
@@ -2,4 +2,6 @@ import os
 
 MODEL_MODE = "ml" # "ml"
 
-LOCAL_MODEL_PATH = "ml/artifacts/spam_model.joblib"
+# LOCAL_MODEL_PATH = "ml/artifacts/spam_model.joblib"
+MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
+MODEL_URI = "runs:/5325c4ed6fba4a4ba05200e851d8571e/model"
